ba0f95741b1e074c36e2ce6e26aa53dd46496fa4
Gagandeip Singh <gagandeips@gmail.com>
2025-02-18 20:36:57 +0530

    Customised PopUp Menu & Bottom Nav

--- app/src/main/java/com/example/tempnavigation/MainActivity.kt
+++ app/src/main/java/com/example/tempnavigation/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.tempnavigation
 
+import android.os.Build
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import com.example.tempnavigation.databinding.ActivityMainBinding
@@ -19,10 +20,15 @@ class MainActivity : AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        window.decorView.setOnApplyWindowInsetsListener { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom - 120)
             insets
+        }
+            val insetsController = window.decorView.windowInsetsController
+            insetsController?.hide(android.view.WindowInsets.Type.systemBars() or android.view.WindowInsets.Type.statusBars())
+            insetsController?.systemBarsBehavior = android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
         val navHost : NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHost.navController
@@ -42,7 +48,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.popupMenu.setOnClickListener {
-            val popupmenu = PopupMenu(this,it)
+            val popupmenu = PopupMenu(this,it,Gravity.END)
             popupmenu.inflate(R.menu.popup_menu)
             popupmenu.show()
 //            popupmenu.menu.getItem(0).setOnMenuItemClickListener {
